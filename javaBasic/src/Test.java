e12ee2b493d4b1f22c529542e25cc629c61e74ce
wu158 <wu15857871029>
2022-12-24 20:04:29 +0800

    这个是3测试

--- javaBasic/src/Test.java
+++ javaBasic/src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("sdadwadawd");
         System.out.println("nmsl");
+        System.out.println("wtf");
     }
 }
