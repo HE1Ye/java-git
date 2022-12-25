037149fcc59b88cf06682a5fa4e9ce9a269a6550
wu158 <wu15857871029>
2022-12-25 21:01:23 +0800

    day4 study

---
+++ study04/src/number/Demo01.java
@@ -0,0 +1,19 @@
+package number;
+
+public class Demo01 {
+    public static void main(String[] args)
+    {
+//     int a=12;
+//     int b=20;
+//        System.out.println(Math.max(a, b));
+//        char b='\\';
+//        Integer a=10;
+//        System.out.println(b);
+//        char a='a';
+        int a=97;
+        char A='A';
+        System.out.println((int) 'a');
+
+
+    }
+}
